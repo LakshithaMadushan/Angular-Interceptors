403eea8ad06d967922eb17ce69631a49036803cd
Lakshitha Madushan <lakshitha199412@gmail.com>
2019-08-20 07:59:20 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Interceptors';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/assets/my.json').subscribe((res) => {
+      console.log(res);
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
